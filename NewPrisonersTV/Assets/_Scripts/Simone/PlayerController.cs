857524c0cbf171c2db49c3edbe48acac6050a2c8
Simone Di Giorgi <simonetomi@gmail.com>
2018-05-25 22:15:04 +0200

    Gun rotation

--- NewPrisonersTV/Assets/_Scripts/Simone/PlayerController.cs
+++ NewPrisonersTV/Assets/_Scripts/Simone/PlayerController.cs
@@ -8,12 +8,11 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb;                                                                     // Rigidbody components
     private Animator anim;
 
+    public Transform gun;
     [BoxGroup("Ground")] public Transform groundCheck;                                          // Player ground collider
     [BoxGroup("Ground")] public LayerMask groundMask;                                           // Ground mask
     [BoxGroup("Ground")] public float checkRadius;                                              // Ground collider radius
     private bool isGrounded;                                                                    // Is the Player on ground?                                                                        
-
-    private float moveInput;                                                                    // Player movements input
 
     [BoxGroup("Controls")] public float speed;                                                  // Player speed
     [BoxGroup("Controls")] public float jump;                                                   // Player jump value
@@ -35,9 +34,10 @@ public class PlayerController : MonoBehaviour {
         // Move inputs
         float moveInput = Input.GetAxis("Horizontal");
 
+        // Movements
         if(moveInput >= joypadDeathZone)                                                        // Move right if "x" axis is over 0.2
             rb.velocity = new Vector2(speed, rb.velocity.y);
-        else if(moveInput <= -joypadDeathZone)                                                  // Move lefet if "x" axis is lover -0.2
+        else if(moveInput <= -joypadDeathZone)                                                  // Move left if "x" axis is lover -0.2
             rb.velocity = new Vector2(-speed, rb.velocity.y);
         else                                                                                    // Stop the player 
             rb.velocity = new Vector2(0, rb.velocity.y);                                        
@@ -67,13 +67,10 @@ public class PlayerController : MonoBehaviour {
             extraJumps--;
         }
         else if(Input.GetButtonDown("Fire1") && extraJumps == 0 && isGrounded)
-        {
             rb.velocity = Vector2.up * jump;
-        }
 
         // Swich between ground bool (Jump)
-        anim.SetBool("Grounded", isGrounded);
-            
+        anim.SetBool("Grounded", isGrounded);         
     }
 
     // Flip the player face method
@@ -84,11 +81,19 @@ public class PlayerController : MonoBehaviour {
 
         if (facingRight)
         {
+            // Rotate the player
             rotation.y = 180;
+
+            // Rotate the weapon
+            gun.transform.localEulerAngles = new Vector3(180, 0, 0);
         }
         else
         {
+            // Rotate the player
             rotation.y = 0;
+
+            // Rotate the weapon
+            gun.transform.localEulerAngles = new Vector3(0, 0, 0);
         }
 
         transform.localEulerAngles = rotation;
--- NewPrisonersTV/Assets/_Scripts/Simone/RotateTriggers.cs
+++ NewPrisonersTV/Assets/_Scripts/Simone/RotateTriggers.cs
@@ -1,11 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class RotateTriggers : MonoBehaviour
 {
-    Text x;
-    Text y;
     PlayerController pc;
 
     public void Start()
@@ -16,21 +13,9 @@ public class RotateTriggers : MonoBehaviour
     void Update()
     {
         JoyRotation();
-        //MouseRotation();
     }
 
-    /*public void MouseRotation()
-    {
-        Vector3 mouse_pos = Input.mousePosition;
-        Vector3 player_pos = Camera.main.WorldToScreenPoint(this.transform.position);
-
-        mouse_pos.x = mouse_pos.x - player_pos.x;
-        mouse_pos.y = mouse_pos.y - player_pos.y;
-
-        float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-    }*/
-
+    // Rotate the Joystick on 360°
     public void JoyRotation()
     {
         Vector3 joyPosition = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
