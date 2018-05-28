4ace7fb3b41564c4ee89ba15b1ac508845176d21
Simone Di Giorgi <simonetomi@gmail.com>
2018-05-28 20:11:21 +0200

    Weapon Sound

--- NewPrisonersTV/Assets/_Scripts/Simone/Weapon.cs
+++ NewPrisonersTV/Assets/_Scripts/Simone/Weapon.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using Sirenix.OdinInspector;
 
 public class Weapon : MonoBehaviour {
+
+    private AudioSource source;
 
     private BoxCollider2D coll;
     private GameObject hand;
@@ -14,12 +17,19 @@ public class Weapon : MonoBehaviour {
     [BoxGroup("Controls")] public float fireRate;
     [BoxGroup("Controls")] public bool autoFire;
 
+    [BoxGroup("Sounds")] public AudioClip shootSound;
+    [BoxGroup("Sounds")] [Range(0.1f, 1f)] public float shootVolume;
+    [BoxGroup("Sounds")] public AudioClip grabSound;
+    [BoxGroup("Sounds")] [Range(0.1f, 1f)] public float grabVolume;
+
     private float lastShot = 0.0f;
 
     [HideInInspector] public bool isGrabbed;
 
     private void Start()
     {
+        source = GetComponent<AudioSource>();
+
         coll = transform.GetChild(1).GetComponent<BoxCollider2D>();
         hand = GameObject.Find("Hand");
 
@@ -57,6 +67,8 @@ public class Weapon : MonoBehaviour {
     {
         if (collision.gameObject.CompareTag("Player_1") && !isGrabbed)
         {
+            source.PlayOneShot(grabSound, grabVolume);
+
             if (hand.transform.childCount <= 1)
             {
                 isGrabbed = true;
@@ -73,8 +85,12 @@ public class Weapon : MonoBehaviour {
     {
         if (Time.time > fireRate + lastShot)
         {
+            source.PlayOneShot(shootSound, shootVolume);
+
             Instantiate(bullet, spawnPoint.transform.position, spawnPoint.transform.rotation);
             lastShot = Time.time;
+
+
         }
     }
 }
