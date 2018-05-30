cfe9d42b91741fd4143b2d5f820ab5e4f32a19d8
Simone Di Giorgi <simonetomi@gmail.com>
2018-05-31 01:11:08 +0200

    Added canvas when player die

---
+++ NewPrisonersTV/Assets/_Scripts/Simone/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Text player1Continue;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (GameObject.FindGameObjectWithTag("Player_1") == null)
+        {
+            player1Continue.enabled = true;
+
+        }
+        else
+        {
+            player1Continue.enabled = false;
+        }
+    }
+}
