72d9bc5478ff94906712e1cd8f537af404731dc1
Simone Di Giorgi <simonetomi@gmail.com>
2018-08-16 22:34:33 +0200

    Main Scene

--- NewPrisonersTV/Assets/_Scripts/Weapons/Bullet.cs
+++ NewPrisonersTV/Assets/_Scripts/Weapons/Bullet.cs
@@ -8,10 +8,6 @@ public class Bullet : MonoBehaviour {
     [BoxGroup("Controls")] public float speed;                                                          // Bullet speed
     [BoxGroup("Controls")] public float destroyAfter;                                                   // Destroy the bullet after seconds
 
-    [BoxGroup("Kind of bullet")] public bool isArrow;                                                   // They block on Walls
-    [BoxGroup("Kind of bullet")] public bool isLaser;                                                   // Move like a laser beam
-    [BoxGroup("Kind of bullet")] public bool isGhost;                                                   // Can surpass walls
-
     protected int damage;
     protected bool destroyOnEnemyCollision;
 
@@ -26,48 +22,33 @@ public class Bullet : MonoBehaviour {
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        // Destroy if touch another bullet
-        if (collision.gameObject.CompareTag("Bullet"))
-            gameObject.SetActive(false);
-
         // Destroy at impact || destroy after seconds
         if (collision.gameObject.CompareTag("Wall"))
         {
-            Debug.Log("Wallll");
-            if (!isArrow)
-            {
-                if (!isGhost && !isLaser)
-                    gameObject.SetActive(false);
-                else if (isGhost)
-                    gameObject.SetActive(false);
-            }
-
-            // Arrow behaviour (Stay stuck on wall)
-            else if (isArrow)
-            {
-                speed = 0;
-                gameObject.SetActive(false);
-            }
-
-            if (isLaser)
-            {
-                // Laser behaviour (Reflect on walls)
-                transform.Rotate(0, 0, -45);
-                transform.right = -transform.right;
-            }
-
-            //gameObject.SetActive(false);
+            Debug.Log("Bullet hit the wall");
         }
 
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            //substract enemy life
+            Debug.Log("You hit the Enemy");
+
+            //Substract enemy life
             Enemy enemyHit = collision.gameObject.GetComponent<Enemy>();
             enemyHit.enemyMembership = membership;
             enemyHit.life -= damage;
 
             if(destroyOnEnemyCollision)
                 gameObject.SetActive(false);
+        }
+
+        if (collision.gameObject.CompareTag("Player_2"))
+        {
+            Debug.Log("You hit Player 2");
+        }
+
+        if (collision.gameObject.CompareTag("Player_1"))
+        {
+            Debug.Log("You hit Player 1");
         }
     }
 }
