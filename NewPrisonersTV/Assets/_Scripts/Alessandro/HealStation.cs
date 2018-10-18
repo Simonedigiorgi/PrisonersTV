18a76f6a90ce8073a46bd4270a4a31537b092e01
Galahad91 <alex.elia.91@gmail.com>
2018-10-18 15:27:39 +0200

    added parameter to choose how much healing the station does to the player

--- NewPrisonersTV/Assets/_Scripts/Alessandro/HealStation.cs
+++ NewPrisonersTV/Assets/_Scripts/Alessandro/HealStation.cs
@@ -7,15 +7,16 @@ public class HealStation : MonoBehaviour
 {
     public float coolDown;
     public int price;
+    [Range(1,3)]public int healAmount;
+
     private float timer;
     private bool avaible = true;
-	// Use this for initialization
+
 	void Start ()
     {
         timer = coolDown;
 	}
 
-    // Update is called once per frame
     void Update ()
     {
         if (GMController.instance.gameStart)
@@ -34,10 +35,10 @@ public class HealStation : MonoBehaviour
     public void UseStation(_CharacterController player)
     {
         if (avaible && GMController.instance.playerInfo[player.playerNumber].score >= price)
-        {
+        { 
             if (player.currentLife < player.m_CharStats.life)
             {
-                player.currentLife++;
+                player.currentLife += healAmount;
                 GMController.instance.playerInfo[player.playerNumber].score -= price;
                 Debug.Log("cured");
             }
